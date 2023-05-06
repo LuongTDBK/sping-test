fcb7fa0be6abe7e7d421344590c62caf8ef7ad75
LuongTDBK <69749518+LuongTDBK@users.noreply.github.com>
2023-05-06 16:42:15 +0700

    add info

--- src/main/java/vn/neo/repository/ManhKMRepository.java
+++ src/main/java/vn/neo/repository/ManhKMRepository.java
@@ -3,4 +3,9 @@ package vn.neo.repository;
 public class ManhKMRepository {
     private String name;
     // Master in here
+    //ADD INFO to main;
+    //ADD INFO to main;
+    //ADD INFO to main;
+    //ADD INFO to main;
+    //ADD INFO to main;
 }
